dccb3e1489438b8459460f0d4051c4b0162b14af
harpreetsc1992 <hsc.10oct@gmail.com>
2018-02-26 23:09:18 -0500

    Cat works; Trying to return to sbush

--- bin/cat/cat.c
+++ bin/cat/cat.c
@@ -13,10 +13,6 @@ main(
 	 char **envp
 	)
 {
-//	__asm__ __volatile__(
-//			"mov %%rbx, %0\t\n"
-//			: "=r" ((uint64_t) argv)
-//			);
 	char str[32];
     strcpy(str, argv[1]);
 	printf ("%s\n", str);
@@ -25,17 +21,10 @@ main(
     char buf[64];
     int j = 0;
     int bytes_read;
-//
-//    while(*name != '\0')
-//    {
-//      str[len++] = *name;
-//		name++;
-//    }
 
     str[len] = '\0';
 
     fd = open(str);
-//    printf("\n %s", fd->filename);
 
     bytes_read = read(fd, buf, 256);
     while (bytes_read-- != 0)
